f473949664664229f131257a520e5cffdb24adce
martin1917 <martin.levin02@mail.ru>
2023-12-29 16:56:07 +0300

    readme

--- 1 laba/main_3.pl
+++ 1 laba/main_3.pl
@@ -3,16 +3,17 @@ use 5.010;
 print "write text: ";
 $text = <STDIN>;
 
-$file_path = "simple.html";
+$file_path = "simple123.html";
 open(my $fd, ">$file_path") or die "file error";
 
 select $fd;
-print "<html>";
-print "<body>";
-print "<h1>$text</h1>";
-print "</body>";
-print "</html>";
-close $fd;
+print<<HTML;
+<html>
+	<body>
+		<h1>$text</h1>
+	</body>
+</html>
+HTML
 
 select STDOUT;
 print "Text has written to $file_path. Check it";
